301774c7bfd29627195adff919b327e2801ecfdf
kumailf <kumailf77@gmail.com>
2023-05-10 15:14:32 +0800

    update

---
+++ src/components/tools/Markdown.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import marked from "marked";
+
+export default {
+    name: "Markdown",
+    props: {
+        markdown: {
+            type: String,
+            required: true,
+        },
+    },
+    computed: {
+        parsedMarkdown() {
+            return marked(this.markdown);
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="markdown-container">
+        <div v-html="parsedMarkdown"></div>
+    </div>
+</template>
+  
+
+
+  
--- src/router/index.ts
+++ src/router/index.ts
@@ -10,11 +10,6 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/chatgpt',
       name: 'chatgpt',
       component: () => import('../views/ChatgptView.vue')

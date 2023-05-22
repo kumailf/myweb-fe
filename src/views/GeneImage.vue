130d7eb7fd2a0ae47f0da942f85f977c1ce259ea
zhengshaofeng <zhengshaofeng@pjlab.org.cn>
2023-05-22 15:49:01 +0800

    fix

--- src/views/GeneImage.vue
+++ src/views/GeneImage.vue
@@ -4,21 +4,21 @@ import axios from 'axios';
 export default {
   data() {
     return {
-        image_prompt: "white cat",
-        image: "",
-        newMsg: "", // 添加一个新消息的数据绑定
-        isDisabled: false, // 添加一个禁用按钮的数据绑定
+      image_prompt: "",
+      image: "",
+      newMsg: "", // 添加一个新消息的数据绑定
+      isDisabled: false, // 添加一个禁用按钮的数据绑定
     }
   },
   methods: {
     geneImg() {
       this.isDisabled = true; // 禁用按钮
-      axios.post('http://www.kumail.moe:8081/api/gene-image', { usermsg: this.image_prompt }, { withCredentials: true })
+      axios.post('http://www.kumail.moe:8081/api/gene-image', { image_prompt: this.image_prompt }, { withCredentials: true })
         .then((response: { data: { image_url: string } }) => {
           console.log(response.data)
           // 将接口返回的数据添加到消息记录中
           this.image = response.data.image_url
-          this.image_prompt = ""; // 清空输入框
+          // this.image_prompt = ""; // 清空输入框
           this.isDisabled = false; // 启用按钮
         })
         .catch((error: any) => {
@@ -33,14 +33,20 @@ export default {
 </script>
 
 <template>
-    <main>
-      <div>
-        <img :src="image" alt="Image" v-if="image" />
-        <p>{{ image_prompt }}</p>
+  <main>
+    <div style="display: flex; justify-content: center;">
+      <div class="form-control">
+        <div class="input-group">
+          <input type="text" placeholder="Describe Your Image" class="input input-lg input-bordered" v-model="image_prompt"
+            @keyup.enter="geneImg()" />
+          <button class="btn btn-lg btn-square" :disabled="isDisabled" @click="geneImg()">Go</button>
+        </div>
       </div>
-      <div>
-        <input type="text" v-model="image_prompt" placeholder="Image Prompt" />
-        <button @click="geneImg" :disabled="isDisabled">Generate Image</button>
-      </div>
-    </main>
-  </template>
+    </div>
+    <p style="display: flex; justify-content: center;">{{ image_prompt }}</p>
+    <div style="display: flex; justify-content: center;">
+      <img :src="image" alt="Image" v-if="image" />
+    </div>
+
+  </main>
+</template>

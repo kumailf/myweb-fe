bd11558231563c222bcc6ab55ec30e4424e92f5a
zhengshaofeng <zhengshaofeng@pjlab.org.cn>
2023-06-12 15:06:28 +0800

    add yabbi

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,6 +5,13 @@ import WelcomeItem from './WelcomeItem.vue'
 <template>
   <WelcomeItem>
     <template #heading>手机用户请点击左上角选功能</template>
-    <a>暂时没想好主页放什么,放个邮箱吧: kumailf77@gmail.com,有反馈或者想要的功能都可以发,后边会陆续上一些新东西</a>
+    <div>
+      <a>暂时没想好主页放什么,放个邮箱吧: kumailf77@gmail.com,有反馈或者想要的功能都可以发,后边会陆续上一些新东西</a>
+    </div>
+    <div>
+      <a>放个友链,亚比日历,获取最新亚比资讯:  </a>
+      <a href="https://www.yabbi.cn/home" style="text-decoration: underline;">https://www.yabbi.cn/home</a>
+    </div>
+
   </WelcomeItem>
 </template>

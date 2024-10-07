7a8a09a084442d436728ccca0ffe0010801abadd
kumailf <kumailf77@gmail.com>
2024-10-08 04:34:44 +0800

    fix

--- src/views/RecommendView.vue
+++ src/views/RecommendView.vue
@@ -12,47 +12,49 @@ export default {
     }
   },
   methods: {
-    submit() {
+    async submit() {
       this.isDisabled = true; // 禁用按钮
-      axios.post('https://www.kumail.moe/api/recommend', {
-        song_title: this.song_title,
-        song_artist: this.song_artist,
-        your_name: this.your_name
-      }, { withCredentials: true })
-        .then((response: { data: { image_url: string } }) => {
-          console.log(response.data)
-          // 将接口返回的数据添加到消息记录中
-          this.image = response.data.image_url
-        })
-        .catch((error: any) => {
-          console.error(error);
-          this.isDisabled = false; // 启用按钮
-          alert("服务挂了,别急,在修了,也有可能是你输入了一些不太policy的关键词")
-        });
-
+      try {
+        const response = await axios.post('https://www.kumail.moe/api/recommend', {
+          song_title: this.song_title,
+          song_artist: this.song_artist,
+          your_name: this.your_name
+        }, { withCredentials: true });
+        // 将接口返回的数据添加到消息记录中
+        console.log(response.data)
+        // 假设接口返回的数据中有 message 字段
+        this.newMsg = response.data.message || "提交成功！";
+        // 清空输入框
+        this.song_title = "";
+        this.song_artist = "";
+        this.your_name = "";
+      } catch (error: any) {
+        console.error(error);
+        this.newMsg = "服务挂了,别急,在修了,也有可能是你输入了一些不太policy的关键词";
+      } finally {
+        this.isDisabled = false; // 启用按钮
+      }
     }
   }
 }
 </script>
 
 <template>
-    <main>
-      <p style="display: flex; justify-content: center;"></p>
-  
-      <div style="display: flex; justify-content: center;">
-        <div class="form-control">
-          <div class="input-group">
-            <input type="text" placeholder="歌名" class="input input-lg input-bordered" v-model="song_title" />
-          </div>
-          <div class="input-group">
-            <input type="text" placeholder="演唱者" class="input input-lg input-bordered" v-model="song_artist" />
-          </div>
-          <div class="input-group">
-            <input type="text" placeholder="你的名字" class="input input-lg input-bordered" v-model="your_name" />
-          </div>
-          <button class="btn btn-lg btn-square" :disabled="isDisabled" @click="submit()">提交</button>
+  <main>
+    <div style="display: flex; justify-content: center;">
+      <div class="form-control">
+        <div class="input-group">
+          <input type="text" placeholder="歌名" class="input input-lg input-bordered" v-model="song_title" />
         </div>
+        <div class="input-group">
+          <input type="text" placeholder="演唱者" class="input input-lg input-bordered" v-model="song_artist" />
+        </div>
+        <div class="input-group">
+          <input type="text" placeholder="你的名字" class="input input-lg input-bordered" v-model="your_name" />
+        </div>
+        <button class="btn btn-lg btn-square" :disabled="isDisabled" @click="submit()">提交</button>
       </div>
-      <p style="display: flex; justify-content: center;">提交成功！</p>
-    </main>
-  </template>
+    </div>
+    <p style="display: flex; justify-content: center;" v-if="newMsg">{{ newMsg }}</p>
+  </main>
+</template>

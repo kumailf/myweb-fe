7799f5ad18018ebca1421c03e929400424e9ce38
kumailf <kumailf77@gmail.com>
2024-10-08 05:10:07 +0800

    fix

--- src/views/RecommendView.vue
+++ src/views/RecommendView.vue
@@ -7,32 +7,31 @@ export default {
       song_title: "",
       song_artist: "",
       your_name: "",
-      newMsg: "", // 添加一个新消息的数据绑定
+      message: "", // 添加一个新消息的数据绑定
       isDisabled: false, // 添加一个禁用按钮的数据绑定
     }
   },
   methods: {
-    submit() {
-      this.isDisabled = true; // 禁用按钮
-      axios.post('https://www.kumail.moe/api/recommend', {
-        song_title: this.song_title,
-        song_artist: this.song_artist,
-        your_name: this.your_name
-      }, { withCredentials: true })
-      .then((response) => {
-        console.log(response.data)
-        // 将接口返回的数据添加到消息记录中
-        this.newMsg = response.data.message || "提交成功！";
-        // 清空输入框
-        this.song_title = "";
-        this.song_artist = "";
-        this.your_name = "";
-      })
-      .catch((error) => {
-        console.error(error);
-        this.newMsg = "服务挂了,别急,在修了";
-        this.isDisabled = false; // 启用按钮
-      });
+    async submit() {
+      this.isDisabled = true;
+
+      try {
+        const response = await axios.post('https://www.kumail.moe/api/recommend', {
+            song_title: this.song_title,
+            song_artist: this.song_artist,
+            your_name: this.your_name
+        }, {
+          withCredentials: true
+        });
+
+        console.log(response.data);
+        this.message = response.data.message;
+
+        this.isDisabled = false;
+      } catch (error) {
+        console.error('请求错误:', error);
+        this.isDisabled = false; // 请求错误时也要启用按钮
+      }
     }
   }
 }
@@ -40,21 +39,28 @@ export default {
 </script>
 
 <template>
-  <main>
-    <div style="display: flex; justify-content: center;">
-      <div class="form-control">
-        <div class="input-group">
-          <input type="text" placeholder="歌名" class="input input-lg input-bordered" v-model="song_title" />
+    <main>
+      <div class="card w-96 bg-base-100 shadow-xl"
+        style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        <div class="card-body">
+          <h2 class="card-title">推歌</h2>
+  
+          <label for="song_title">歌名:</label>
+          <input v-model="song_title" type="text" id="song_title" placeholder="歌名">
+          
+          <label for="song_artist">演唱者:</label>
+          <input v-model="song_artist" type="text" id="song_artist" placeholder="演唱者">
+          
+          <label for="your_name">推荐人:</label>
+          <input v-model="your_name" type="text" id="your_name" placeholder="推荐人">
+
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary" :disabled="isDisabled" @click="submit">提交</button>
+          </div>
+  
+          <p v-if="message">{{ message }}</p>
         </div>
-        <div class="input-group">
-          <input type="text" placeholder="演唱者" class="input input-lg input-bordered" v-model="song_artist" />
-        </div>
-        <div class="input-group">
-          <input type="text" placeholder="你的名字" class="input input-lg input-bordered" v-model="your_name" />
-        </div>
-        <button class="btn btn-lg btn-square" :disabled="isDisabled" @click="submit()">提交</button>
       </div>
-    </div>
-    <p style="display: flex; justify-content: center;" v-if="newMsg">{{ newMsg }}</p>
-  </main>
-</template>
+  
+    </main>
+  </template>
